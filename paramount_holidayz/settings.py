2859cc1ba09a42568d6ccab8fab1bc4c63ad01a1
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2026-01-15 16:35:56 +0530

    Update settings.py

--- paramount_holidayz/settings.py
+++ paramount_holidayz/settings.py
@@ -25,10 +25,17 @@ SECRET_KEY = 'django-insecure-iv+1l8$m&-!0&f22g(tv#6x6!r+&^vb_ih7yw_&#0rg1v#%2ci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://paramountholidayz.com','paramount-holidayz-production.up.railway.app']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'paramountholidayz.com',
+    'www.paramountholidayz.com',
+    'paramount-holidayz-production.up.railway.app'
+]
 CSRF_TRUSTED_ORIGINS = [
     'https://paramount-holidayz-production.up.railway.app',
-    "https://paramountholidayz.com",
+    'https://paramountholidayz.com',
+    'https://www.paramountholidayz.com',
 ]
 
 
@@ -39,9 +46,9 @@ X_FRAME_OPTIONS = 'DENY'
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-SECURE_SSL_REDIRECT = False  # Set to True in production with HTTPS
-SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
-CSRF_COOKIE_SECURE = False  # Set to True in production with HTTPS
+SECURE_SSL_REDIRECT = True  # Set to True in production with HTTPS
+SESSION_COOKIE_SECURE = True  # Set to True in production with HTTPS
+CSRF_COOKIE_SECURE = True  # Set to True in production with HTTPS
 SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
 
 # Email Configuration
